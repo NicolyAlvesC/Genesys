77d1bdda85a79bf87744dc3ae6b05a62bfcd5675
NicolyALvesC <niconicoly9718@gmail.com>
2023-11-20 15:08:31 -0300

    ok

--- src/pages/Services/styled.js
+++ src/pages/Services/styled.js
@@ -49,18 +49,6 @@ const ConteinerService = styled.div`
   margin-top: 1em;
 }
 
-<<<<<<< HEAD
-.page {
-  display: grid;
-  padding-top: 5em;
-  padding-left: 1em;
-  font-family: Arial, sans-serif;
-  grid-gap: 10px;
-  grid-template-areas:
-    'header header header header header header'
-    'menu main main main right right'
-    'menu footer footer footer footer footer';
-=======
 img {
   border: 2px solid #FF6B;
 }
@@ -73,7 +61,6 @@ img {
   height: 50vh;
 
   margin: 2.5em 2.5em 0em 2.5em !important;
->>>>>>> d7f5e39ba45a35cd4ce39595714360e8517bb7cc
 }
 
 .header-1, .header-2, .header-3 {
@@ -98,52 +85,6 @@ img {
   height: 23vh;
 }
 
-<<<<<<< HEAD
-/* Dropdown Button */
-.dropbtn {
-  background-color: #3498DB;
-  color: white;
-  padding: 16px;
-  font-size: 16px;
-  border: none;
-  cursor: pointer;
-}
-
-/* Dropdown button on hover & focus */
-.dropbtn:hover, .dropbtn:focus {
-  background-color: #2980B9;
-}
-
-/* The container <div> - needed to position the dropdown content */
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-/* Dropdown Content (Hidden by Default) */
-.dropdown-content {
-
-  position: absolute;
-  background-color: #f1f1f1;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-/* Links inside the dropdown */
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-/* Change color of dropdown links on hover */
-.dropdown-content a:hover {background-color: #ddd;}
-
-/* Show the dropdown menu (use JS to add this class to the .dropdown-content container when the user clicks on the dropdown button) */
-.show {display:block;}
-=======
 .maior-baixo {
   width: 25vw;
   height: 23vh;
@@ -161,6 +102,5 @@ img {
 } 
 
 
->>>>>>> d7f5e39ba45a35cd4ce39595714360e8517bb7cc
 `
 export {ConteinerService}
